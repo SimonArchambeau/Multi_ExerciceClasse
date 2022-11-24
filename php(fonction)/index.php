d524a7ab698bc54591a8368ff032e22cb6c40f4b
Archambeau Simon <210303@CCM-Asty-Moulin.local>
2022-11-24 14:29:56 +0100

    mise a jour

---
+++ php(fonction)/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simon Archambeau</title>
+</head>
+<body>
+
+<?php
+    $n = 5;
+    function fact($n){ 
+        $f = 1; 
+        for ($i = 1; $i <= $n; $i++){ 
+            $f = $f * $i; 
+        } 
+        return $f; 
+    } 
+
+    function occurence($z){
+        $l = 0;
+        for (i = 0, i<= )
+        
+    }
+    
+
+    $f = fact($n); 
+?>
+
+
+<p>la factorielle de <?= $n ?> vaut <?= fact($n) ?></p>
+</body>
+</html>
